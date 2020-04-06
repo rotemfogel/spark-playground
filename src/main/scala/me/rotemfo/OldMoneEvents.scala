c4aebabd49403e25d64f6cd7e5b5c63412be20f0
Rotem Fogel <rotem@seekingalpha.com>
2020-04-06 17:03:28 +0300

    :}

--- src/main/scala/me/rotemfo/OldMoneEvents.scala
+++ src/main/scala/me/rotemfo/OldMoneEvents.scala
@@ -7,49 +7,58 @@ import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructT
 //noinspection ScalaCustomHdfsFormat
 object OldMoneEvents extends App {
 
-  case class Year(prefix: String, year: Int, computeMax: Boolean = false)
+  case class ParseYear(prefix: String, year: Int, computeMax: Boolean = false)
 
   val spark = SparkSession.builder().appName(getClass.getSimpleName).master("local[*]").getOrCreate()
 
-  def parseYear(y: Year): Unit = parseYear(y.prefix, y.year, y.computeMax)
+  val emailCol = "email"
+  val userIdCol = "user_id"
+  val tsCol = "ts"
+
+  def read(prefix: String, year: Int, computeMax: Boolean): DataFrame = {
+    val (tmpUserIdCol, tmpEmailCol, tmpTsCol) = year match {
+      case y: Int if y < 2015 => ("_c36", "_c38", "_c1")
+      case y: Int if y >= 2015 && y <= 2017 => ("_c32", "_c34", "_c1")
+      case 2018 => ("_c27", "_c28", "_c2")
+    }
+    val df = spark.read
+      .format("com.databricks.spark.csv")
+      .option("header", false)
+      .option("delimiter", "\t")
+      .csv(s"/data/${prefix}_${year}*.*")
+      .where(col(tmpUserIdCol).isNotNull)
+      .withColumnRenamed(tmpEmailCol, emailCol)
+      .withColumnRenamed(tmpUserIdCol, userIdCol)
+    if (computeMax) {
+      df.withColumn(tsCol, to_timestamp(col(tmpTsCol), "yyyy-MM-dd HH:mm:SS")).select(userIdCol, emailCol, tsCol).groupBy(userIdCol, emailCol).agg(max(tsCol))
+    } else {
+      df.select(userIdCol, emailCol).distinct
+    }
+  }
 
   def parseYear(prefix: String, year: Int, computeMax: Boolean = false): Unit = {
-    val df = spark.read.format("com.databricks.spark.csv").option("header", false).option("delimiter", "\t").csv(s"/data/${prefix}_${year}*.*")
-    val (userIdCol, emailCol, tsCol) = year match {
-      case y: Int if y < 2015 => ("_c36", "_c38", "_c2")
-      case 2015 => ("_c32", "_c34", "_c2")
-      case 2016 => ("_c33", "_c35", "_c2")
-      case 2017 => ("_c26", "_c27", "_c2")
-      case 2018 => ("_c28", "_c29", "_c2")
-    }
-    val whereDf = df.where(col(userIdCol).isNotNull)
-    val writeDf = if (computeMax) {
-      whereDf.withColumn("ts", to_timestamp(col(tsCol), "yyyy-MM-dd HH:mm:SS")).select(userIdCol, emailCol, "ts").groupBy(userIdCol, emailCol).agg(max("ts"))
-    } else {
-      whereDf.select(userIdCol, emailCol).distinct
-    }
-    writeDf.coalesce(1).write.mode(SaveMode.Overwrite).format("com.databricks.spark.csv").option("header", false).save(s"/data/output/${year}")
+    val df = read(prefix, year, computeMax)
+    df.coalesce(1).write.mode(SaveMode.Overwrite).format("com.databricks.spark.csv").option("header", false).save(s"/data/output/${year}")
+    df.unpersist
   }
+
+  def parse(y: ParseYear): Unit = parseYear(y.prefix, y.year, y.computeMax)
 
   val schema = StructType(Seq(StructField("user_id", IntegerType, nullable = false), StructField("email", StringType, nullable = false)))
 
-  def read(year: Int): DataFrame = {
-    spark.read.format("com.databricks.spark.csv").option("header", false).option("delimiter", ",").schema(schema).csv(s"/data/output/$year/*.csv").withColumn("year_", lit(year))
-  }
-
   val years = Seq(
-    Year("mone", 2012),
-    Year("mone", 2013),
-    Year("mone", 2014),
-    Year("dw_mone", 2015),
-    Year("dw_mone", 2016),
-    Year("dw_mone", 2017),
-    Year("dw_mone", 2018)
+    ParseYear("mone", 2012, computeMax = true),
+    ParseYear("mone", 2013, computeMax = true),
+    ParseYear("mone", 2014, computeMax = true),
+    ParseYear("dw_mone", 2015, computeMax = true),
+    ParseYear("dw_mone", 2016, computeMax = true),
+    ParseYear("dw_mone", 2017, computeMax = true),
+    ParseYear("dw_mone", 2018, computeMax = true)
   )
-  years.foreach(parseYear)
+  years.foreach(parse)
 
-  val df = read(2012).union(read(2013)).union(read(2014)).union(read(2015)).union(read(2016))
-  val agg = df.select("user_id", "email").distinct()
-  agg.coalesce(1).write.mode(SaveMode.Overwrite).format("com.databricks.spark.csv").option("header", false).save("/data/output/final")
+  //  val df = read(2012).union(read(2013)).union(read(2014)).union(read(2015)).union(read(2016))
+  //  val agg = df.select("user_id", "email").distinct()
+  //  agg.coalesce(1).write.mode(SaveMode.Overwrite).format("com.databricks.spark.csv").option("header", false).save("/data/output/final")
 
 }
